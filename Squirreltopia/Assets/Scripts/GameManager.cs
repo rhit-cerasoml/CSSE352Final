f2719b46adec4e9e13231a864d135ccdcd956e95
rhit-cerasoml <cerasoml@rose-hulman.edu>
2023-05-18 13:54:35 -0400

    migrate from text to TMP and added housing info text + icon

--- Squirreltopia/Assets/Scripts/GameManager.cs
+++ Squirreltopia/Assets/Scripts/GameManager.cs
@@ -1,26 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour {
 
     [SerializeField] public GameObject nutCountText;
+    [SerializeField] public GameObject capacityText;
 
     private int nut_count;
+    private int squirrel_count;
+    private int squirrel_max_count;
     public int nuts {
         get{
             return nut_count;
         }
         set{
             nut_count = value;
-            nutCountText.GetComponent<Text>().text = nut_count.ToString();    
+            nutCountText.GetComponent<TMP_Text>().text = nut_count.ToString();    
         }
     }
+    public int squirrels {
+        get{
+            return squirrel_count;
+        }
+        set{
+            squirrel_count = value;
+            capacityText.GetComponent<TMP_Text>().text = squirrel_count.ToString() + "/" + squirrel_max_count.ToString();    
+        }
+    }
+    public int capacity {
+        get{
+            return squirrel_max_count;
+        }
+        set{
+            squirrel_max_count = value;
+            capacityText.GetComponent<TMP_Text>().text = squirrel_count.ToString() + "/" + squirrel_max_count.ToString();    
+        }
+    }
+
+
 
     // Start is called before the first frame update
     void Start() {
         nuts = 10;
+        squirrels = 5;
+        capacity = 10;
     }
 
     // Update is called once per frame
