a7c004b6d1919a97dbb16b1b729d735f2d4d4baf
rhit-cerasoml <cerasoml@rose-hulman.edu>
2023-05-18 13:13:58 -0400

    Added first room, might need to clean up textures somehow

--- Squirreltopia/Assets/Scripts/BuildManager.cs
+++ Squirreltopia/Assets/Scripts/BuildManager.cs
@@ -2,18 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BuildManager : MonoBehaviour
-{
+public class BuildManager : Singleton<BuildManager> {
+
+    [SerializeField] GameObject trunkRoom;
+    [SerializeField] GameObject treePrefab;
+    [SerializeField] GameObject[] roomPrefabs;
+    
+    GameObject _tree;
+
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         
     }
 
+    public void Setup(){
+        
+        _tree = Instantiate(treePrefab);
+        _tree.transform.position = new Vector3(0, 0, 0);
+        
+        RoomTemplate room_template = trunkRoom.transform.GetComponent<RoomTemplate>();
+        room_template.Stamp(0, 0, _tree.transform.GetChild(0));
+    }
+    
+    public void Cleanup() {
+        Destroy(_tree);
+    }
+
     // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetMouseButtonDown(0)){
+    void Update() {
+        if(Input.GetMouseButtonDown(0)) {
             Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Debug.Log(worldPosition);
         }
--- Squirreltopia/Assets/Scripts/RoomTemplate.cs
+++ Squirreltopia/Assets/Scripts/RoomTemplate.cs
@@ -8,20 +8,13 @@ public class RoomTemplate : MonoBehaviour {
     [SerializeField] public int height;
     [SerializeField] public int width;
 
-    [SerializeField] public int entrance_height;
-    [SerializeField] public int exit_height;
-
-    [SerializeField] public Vector2Int[] spawns;
-
     public void Stamp(int x_offset, int y_offset, Transform destination){
         Transform grid = transform.GetChild(0);
         Tilemap foreground = grid.GetChild(0).gameObject.GetComponent<Tilemap>();
-        Tilemap midground = grid.GetChild(1).gameObject.GetComponent<Tilemap>();
-        Tilemap background = grid.GetChild(2).gameObject.GetComponent<Tilemap>();
+        Tilemap background = grid.GetChild(1).gameObject.GetComponent<Tilemap>();
         
         Tilemap D_foreground = destination.GetChild(0).gameObject.GetComponent<Tilemap>();
-        Tilemap D_midground = destination.GetChild(1).gameObject.GetComponent<Tilemap>();
-        Tilemap D_background = destination.GetChild(2).gameObject.GetComponent<Tilemap>();
+        Tilemap D_background = destination.GetChild(1).gameObject.GetComponent<Tilemap>();
 
         for(int x = 0; x < width; x++){
             for(int y = 0; y < height; y++){
@@ -31,24 +24,11 @@ public class RoomTemplate : MonoBehaviour {
                 if(tile != null){
                     D_foreground.SetTile(D_pos, tile);
                 }
-                tile = midground.GetTile(pos);
-                if(tile != null){
-                    D_midground.SetTile(D_pos, tile);
-                }
                 tile = background.GetTile(pos);
                 if(tile != null){
                     D_background.SetTile(D_pos, tile);
                 }
             }
-        }
-    }
-
-    public void GenerateSpawns(int x_offset, int y_offset, GameObject[] prefabs, List<GameObject> enemies){
-        for(int i = 0; i < spawns.Length; i++){
-            Vector3 pos = new Vector3(x_offset + spawns[i].x - 0.5f, y_offset + spawns[i].y + 0.5f, 1.0f);
-            GameObject enemy = Instantiate(prefabs[Random.Range(0, prefabs.Length)]);
-            enemy.transform.position = pos;
-            enemies.Add(enemy);
         }
     }
 }
